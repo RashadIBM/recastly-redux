e2e4543efa4cb19fd749324a1eb539cd6f936047
Girish Venkataraman <venkg01@gmail.com>
2019-02-28 23:54:19 -0500

    Complete nav container

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,7 +2,33 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+var mapStateToProps = (state) => {
+  return {
+    videos: state.videos,
+    currentVideo: state.currentVideo
+  };
+};
+
+var mapDispatchToProps = (query) => {
+  return { handleSearchInputChange: handleSearchChange(query) };
+};
+
+var SearchContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
+
+
+// function mapStateToProps (Search.state) {
+//   return {example: Search.state};
+// };
+// var mapDispatchToProps = {};
+
+// var SearchContainer = connect(
+//   mapStateToProps, mapDispatchToProps, Search
+// );
+
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
